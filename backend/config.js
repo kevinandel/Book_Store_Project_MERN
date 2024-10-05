7399c9465520e05fb4b525bf384dcf2c3302d0c9
Kevin Andel Josy <kevinandeljosy11@gmail.com>
2024-10-05 14:50:14 +0530

    Did the project till implementing cors policy

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 
+    'mongodb+srv://kevinandel:Giftofgod%4011@bookstoremern.ok7vsia.mongodb.net/?retryWrites=true&w=majority&appName=BookstoreMERN'
